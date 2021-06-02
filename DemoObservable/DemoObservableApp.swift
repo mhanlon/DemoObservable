c6edbd3100966f57b66f8db5e0e38fd2040c6449
Matt Hanlon <mhanlon@thecodehub.ie>
2021-06-02 06:22:00 +0100

    Initial Commit

---
+++ DemoObservable/DemoObservableApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoObservableApp.swift
+//  DemoObservable
+//
+//  Created by Matthew Hanlon on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoObservableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
